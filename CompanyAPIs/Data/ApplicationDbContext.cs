fb79a942bddf7dccfed52dc0700f32d51e54db7a
MinaHrcom <Mina.M@hrcom.io>
2024-04-25 01:33:22 +0200

    Add project files.

---
+++ CompanyAPIs/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CompanyAPIs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        
+        { 
+        
+        }
+
+    }
+}
